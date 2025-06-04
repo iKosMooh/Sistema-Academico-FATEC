9004c589e252de2684865b66e9c9876bf33421f5
henrique silva <henrique989151620@gmail.com>
2025-06-03 22:21:03 -0300

    blablabla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import SubMenu from './components/SubMenu/SubMenu';
 
 export default function Home() {
   return (
-      <Header />
+
+    <> <Header />
+          <SubMenu />
+      <Footer /> 
+    </>
   );
 }
